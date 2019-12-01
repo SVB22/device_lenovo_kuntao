6f373244360ddc0725714967221388ac025c8500
SVB22 <svb8691@gmail.com>
2019-12-01 15:15:53 +0300

    adb_debug

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -166,3 +166,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=SVB
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
